65404af7d9f7a9cd4607766571173a3973414ff5
Rabab-Soomro <nalishba76@gmail.com>
2024-05-26 22:11:02 +0500

    Add files via upload

---
+++ rabab.js
@@ -0,0 +1,6 @@
+//Q2:Name Cases:store a person's name in a variable,and then print that person's name in lowercase,uppercase and titlecase.
+var personName = "Rabab Soomro";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log("rabab soomro\nRABAB SOOMRO\nRabab soomro");
